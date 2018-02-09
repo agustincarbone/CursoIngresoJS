e7eadb28c1ca40ef162f2adaf21294a8ecf21c1c
agustincarbone <36281596+agustincarbone@users.noreply.github.com>
2018-02-08 22:11:35 -0300

    ejercicio 6
    incompleto

---
+++ jsEntradaSalida-6.js
@@ -0,0 +1,15 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+Mostar el resulto por medio de "ALERT"*/
+function sumar()
+{
+var numero1;
+var numero2;
+
+numero1=document.getElementById('numeroUno').value;
+numero2=document.getElementById('numeroDos').value;
+
+
+
+}
+
